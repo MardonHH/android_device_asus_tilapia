c1afab51ab9fc7e9bc649d32af394685a73bd2ac
Mardon <markusmenzel@gmail.com>
2014-10-02 13:23:43 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,7 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo aosp_tilapia-userdebug
-add_lunch_combo full_tilapia-userdebug
 add_lunch_combo merk_tilapia-userdebug
 
